15bc3037db22010a3c20597b04c09d131cccbc1f
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-07-20 02:08:29 +0100

    automating context with redux

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,5 @@
-import React, { useReducer } from 'react';
+import createDataContext from './createDataContext'
 
-const BlogContext = React.createContext();
 
 const blogReducer = (state, action) => {
     switch (action.type) {
@@ -11,19 +10,14 @@ const blogReducer = (state, action) => {
     }
 }
 
-export const BlogProvider = ({ children }) => {
-    const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-    const addBlogPost = () => {
-        dispatch({ type: 'add_blogpost' })
-    }
-
-
-    return (
-        <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-            {children}
-        </BlogContext.Provider>
-    )
+const addBlogPost = dispatch => {
+    return () => {
+        dispatch({ type: 'add_blogpost' });
+    };
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(
+    blogReducer,
+    { addBlogPost },
+    []
+);
