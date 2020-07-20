f80fc3471e3aaf168446f21424ccc893c95f2580
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-07-20 02:39:38 +0100

    adding delete post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,8 +3,10 @@ import createDataContext from './createDataContext'
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'delete_blogpost':
+            return state.filter((blogPost) => blogPost.id !== action.payload)
         case 'add_blogpost':
-            return [...state, { title: `Blog Post #${state.length + 1}` }]
+            return [...state, { id: Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}` }]
         default:
             return state;
     }
@@ -16,8 +18,14 @@ const addBlogPost = dispatch => {
     };
 };
 
+const deleteBlogPost = dispatch => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id })
+    }
+}
+
 export const { Context, Provider } = createDataContext(
     blogReducer,
-    { addBlogPost },
+    { addBlogPost, deleteBlogPost },
     []
 );
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,19 +1,26 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-    const { state, addBlogPost } = useContext(Context)
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context)
 
     return (
         <View>
-            <Text>Index Screen</Text>
             <Button title="Add Post" onPress={addBlogPost} />
             <FlatList
                 data={state}
                 keyExtractor={blogPost => blogPost.title}
                 renderItem={({ item }) => {
-                    return <Text>{item.title}</Text>
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <Feather style={styles.icon} name="trash" />
+                            </TouchableOpacity>
+                        </View>
+                    )
                 }}
             />
         </View>
@@ -21,7 +28,20 @@ const IndexScreen = () => {
 };
 
 const styles = StyleSheet.create({
-
+    row: {
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
 });
 
 export default IndexScreen;
